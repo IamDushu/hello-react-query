83236f8625c6e4d0a0270c440de38e2bf73f7125
IamDushu <hey.dushyanth@gmail.com>
2025-01-11 15:21:25 +0530

    Book Selector - Managing Query Dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/prop-types */
 import { useQuery } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 
 const BASE_URL = "https://library-api.uidotdev.workers.dev";
 
-async function getData() {
-  const url = `${BASE_URL}/books/pD6arNyKyi8C`;
+async function getData(bookId) {
+  const url = `${BASE_URL}/books/${bookId}`;
   const response = await fetch(url);
 
   // If an error is thrown then isError is set to true
@@ -15,15 +18,15 @@ async function getData() {
   return data;
 }
 
-function useBook() {
+function useBook(bookId) {
   return useQuery({
-    queryKey: ["bookData"],
-    queryFn: getData,
+    queryKey: ["bookData", bookId],
+    queryFn: () => getData(bookId),
   });
 }
 
-function Book() {
-  const { data, isLoading, isError } = useBook();
+function Book({ bookId }) {
+  const { data, isLoading, isError } = useBook(bookId);
 
   if (isLoading) {
     return <Loading />;
@@ -57,14 +60,30 @@ function Error() {
 }
 
 export default function App() {
+  const [selectedBookId, setSelectedBookId] = useState("pD6arNyKyi8C");
+
   return (
-    <div>
-      <header className="app-header">
-        <h1>
-          <span>Query Library</span>
-        </h1>
-      </header>
-      <Book />
-    </div>
+    <>
+      <div>
+        <header className="app-header">
+          <h1>
+            <span>Query Library</span>
+          </h1>
+          <div className="select">
+            <select
+              value={selectedBookId}
+              onChange={(e) => setSelectedBookId(e.target.value)}
+            >
+              <option value="pD6arNyKyi8C">The Hobbit</option>
+              <option value="aWZzLPhY4o0C">The Fellowship Of The Ring</option>
+              <option value="12e8PJ2T7sQC">The Two Towers</option>
+              <option value="WZ0f_yUgc0UC">The Return Of The King</option>
+            </select>
+          </div>
+        </header>
+        <Book bookId={selectedBookId} />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
